807eea2bd2eed575e2ca1c43fe9fb885d03aff70
bhaskar sengupta <53405610+kudos-dot-com@users.noreply.github.com>
2020-08-20 19:06:58 +0530

    Update TodoItem.js

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+//Climbing the tree component drilling
 export class TodoItem extends Component {
     getState=()=>{
         
